105f01ac634ee6aad0e97044621d62905dc9d88c
fazi1023 <fazekhan2305@gmail.com>
2024-10-28 22:22:52 +0000

    e

---
+++ basics/04_comparision.js
@@ -0,0 +1,12 @@
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2 >=1)
+// console.log(2<=1)
+// console.log(2==1)
+// console.log(2!=1)
+
+console.log("2" == 2)
+//cannot compaire the differnrt data types
+//strick check 
+console.log("2" === 2);
+// at === data type also check that why the both data type and value not save
